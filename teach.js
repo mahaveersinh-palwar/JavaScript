b9c26f182d0b6bb3fed41c7647597eb9254d2cd2
unknown <mahaveerrajput84@gmail.com>
2024-12-09 17:38:45 +0530

    Basic Js 9/12/24

---
+++ teach.js
@@ -0,0 +1,216 @@
+// function add()
+// {
+//     let a =10;
+//     let b =20;
+
+//     let sum = a +b ;
+//     // document.write(sum);
+//     // alert(sum); //Display Alert Box
+
+// }
+
+// function variable()
+// {
+//   let car = "Maruti";
+//   window.alert(car);
+// }
+
+
+// for (let i = 0; i<= 10; i++)
+//     {
+//        console.log(i);
+//     }
+
+
+// let ary =['a','b','c','d'];
+// let find = 'a'
+// for(find in ary)
+// {
+//     console.log('Yes');
+// }
+
+// var ary =['a','b','c','d'];
+// var ap = "";
+
+// for (var x of ary)
+// {
+//     ap += x;
+//     console.log(x);
+// }
+
+// var st = "Abcdser";
+ 
+// for (var x of st)
+// {
+//     console.log(x);
+// }
+
+// var a = 10;
+// while(a > 0)
+// {
+//     console.log(a);
+//     a--;
+// }
+
+// var x =10;
+
+// do{
+//     console.log(x);
+//     x--;
+// }
+// while(x > 0);
+
+
+// for (var i=0; i<10; i++)
+// {
+//    if(i == 5)
+//    {break;}
+//    console.log(i);
+// }
+
+
+// for (var i=0; i<10; i++)
+// {
+//    if(i == 5)
+//    {continue;}
+//    console.log(i);
+// }
+
+// var num = 15;
+// var count = 0 ;
+// for(var i = 2; i < num; i++)
+// {
+//  if(num % i == 0)
+//  {
+//     count++;
+//     break;
+//  }
+
+// }
+// if(count == 0)
+// {
+//     console.log("Prime");
+// }
+// else{
+//     console.log("Not Prime...")
+
+// }
+
+// var a = 10;
+// var b = 50;
+// var c = 40;
+// var d = 0;
+
+// if(d == 0)
+// {
+//     if(a > b && a > c)
+//     {
+//         console.log("A Is Big...");
+//     }
+//     else if(b > a && b > c)
+//     {
+//         console.log("B IS Big...");
+//     }
+//     else
+//     {
+//         console.log("C Is Big...");
+//     }
+// }
+// else
+// {
+//     if(a < b && a < c)
+//     {
+//         console.log("A Is Small");
+//     }
+//     else if(b < a && b < c)
+//     {
+//         console.log("B Is Small...");
+//     }
+//     else{
+//         console.log("C Is Small");
+//     }
+// }
+
+// var a = 10;
+// var b = 20;
+// var c ;
+
+// console.log("Before Swapping...");
+// console.log(a);
+// console.log(b);
+// c = a;
+// a = b;
+// b = c;
+// console.log("After Swapping...");
+// console.log(a);
+// console.log(b);
+
+// const getWord=(num)=>{
+//     switch(num){
+//         case 0:
+//             return "Zero";
+//         case 1:
+//             console.log("inside One")
+//                 return "One";
+//         case 2:
+//             console.log("inside Two")
+           
+//                 return "Two";
+//         }
+// }
+// console.log(getWord(1))
+// function myfun(a , b)
+// {
+//      return a + b; 
+
+// }
+
+// addition = myfun(10,5);
+// // console.log(myfun(10,5));
+// console.log(addition);
+
+// function myfun(a , b)
+// {
+//      sum = a + b; 
+//     console.log(sum);
+// }
+// myfun(10,5);
+
+
+// function myfun()
+//     {
+//         console.log("Hello");
+//     }
+//     myfun();
+
+//  function square( a )
+//  {
+//     console.log(a * a);
+//  }
+
+//  square(7);
+
+// function sb()
+// {
+//     var a = 10;
+//     var b = 20;
+//     return [a , b];
+// }
+
+// console.log(sb())
+
+// function cube(a)
+// {
+//     cb = a * a * a; 
+//     console.log(cb)
+// }
+
+// cube(5);
+
+// function myfun(a,b)
+// {
+//     sum = a+b;
+//     return sum; 
+// }
+
+// console.log(myfun(10,5));
